3fcceef38defc114f4811fc74fff075b5324e333
Geethashree-02 <101503046+Geethashree-02@users.noreply.github.com>
2022-08-15 01:41:52 -0700

    WithoutArgswithReturn

---
+++ returnWithoutArgs.py
@@ -0,0 +1,9 @@
+class maths:
+    def add(self):
+        num1 = int(input("Enter the number : "))
+        num2 = int(input("Enter the number : "))
+        sum = num1+num2
+        return sum
+obj = maths()
+plus=obj.add()
+print(plus)
