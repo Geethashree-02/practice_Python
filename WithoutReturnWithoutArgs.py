e1313f0d27ce391c2ce246fa8d4af70ed2a1bb15
Geethashree-02 <101503046+Geethashree-02@users.noreply.github.com>
2022-08-15 02:27:38 -0700

    WithoutReturnWithoutArgs

---
+++ WithoutReturnWithoutArgs.py
@@ -0,0 +1,10 @@
+class maths:
+    def add(self):
+        Num1=int(input("Enter the valid number : "))
+        Num2=int(input("Enter the valid number : "))
+        sum = Num1+Num2
+        print(sum)
+
+obj = maths()
+obj.add()
+
